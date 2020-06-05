6b870286c0090a147641a1d0199178798f6f2126
DeltaForce14 <dita@ditapesek.co.uk>
2020-06-05 12:25:12 +0100

    updated loader

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const totalInterestOut = document.getElementById("total-interest");
 
 
 
-
 loadEventListeners();
 
 function loadEventListeners(){
@@ -25,33 +24,48 @@ function loadEventListeners(){
 
 
 function clickCalcBtn(event){
+    //when the button is reclicked, hide the results
+    //when the page is loaded for the first time they are hiden by default via CSS
+    document.getElementById('results').style.display = "none";
+
     // User Input Variables
 
     const loanValue = parseFloat(loanAmount.value);
     const interestValue = parseFloat(interestPerc.value);
     const yearsToPayValue = parseFloat(paymentYears.value);
+    
+    // Output calculations
+    let monthlyInterestRate = interestValue/100/12;
+    let calculatedPayments = yearsToPayValue * 12;
 
-    let totalPayment = loanValue + ((loanValue/100*interestValue)*yearsToPayValue);
-    let monthlyPayments = (totalPayment/yearsToPayValue)/12;
-    let totalInterest =  totalPayment-loanValue;
+    const monthlyx = Math.pow(1 + monthlyInterestRate, calculatedPayments);
+    const monthlyPayments = (loanValue * monthlyx*monthlyInterestRate)/(monthlyx-1);
 
     //UI outputs
 
     if(isFinite(monthlyPayments)){
         monthlyPayOut.value = monthlyPayments.toFixed(2);
-        totalPayOut.value = totalPayment.toFixed(2);
-        totalInterestOut.value = totalInterest.toFixed(2);
-        
+        totalPayOut.value = (monthlyPayments * calculatedPayments).toFixed(2);
+        totalInterestOut.value = ((monthlyPayments * calculatedPayments)-loanValue).toFixed(2);
+
+        document.getElementById('loading').style.display = "block";
+        setTimeout(showResults,2000);
     }
     else {
-
         showError("Please check if you entered correct numbers.");
     }
 
     event.preventDefault();
 }
 
+// Show results function from Timeout 
+function showResults(){
+    document.getElementById('loading').style.display = "none";
+    document.getElementById('results').style.display = "block";
+}
+
 function showError(message){
+
         //create div
         const newDiv = document.createElement('div');
         //add Bootstrap alert class
@@ -71,6 +85,6 @@ function showError(message){
 
 function clearError(){
     document.querySelector(".alert").remove();
-}
+};
 
 
